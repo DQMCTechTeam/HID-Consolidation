3f6bcaa9dbf9b6a6604b73e0204b9664d9c36f1d
Walter Walden <Waldman200036@gmail.com>
2022-09-21 17:50:33 -0500

    Add project files.

---
+++ HIDFile/sql/Section E/SectionECheck.sql
@@ -0,0 +1,2 @@
+﻿USE HID
+select * from [HID] h where substring(h.QuestionNum,1,1)='e' order by h.DMISID, h.QuestionNum
---
+++ PastDueArrivalsCheck.sql
@@ -0,0 +1,2 @@
+﻿USE HID
+select * from [HID] h where substring(h.QuestionNum,1,5)='B.8.B' order by h.DMISID, h.QuestionNum
