90692d119c6255a654b791b7125b1f4f7b9c931f
Walter Walden <Waldman200036@gmail.com>
2023-03-15 14:50:06 -0500

    Consolidation
    Consolidation

---
+++ HIDFile/SQL Server/Step 1 - Truncate Data.sql
@@ -0,0 +1,11 @@
+﻿Truncate table UploadByMTFs
+
+Truncate table  SectionETotals
+
+Truncate Table PastDueArrivals
+
+Truncate Table HID
+
+Truncate Table CorrectionFlags
+
+Truncate Table LaborCostAccounting
